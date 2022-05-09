ed462420dd43d0b2e1a08df885085d9f0ed5c897
Mikel Diaz <85890651+SBolivarLoL@users.noreply.github.com>
2022-05-09 00:06:36 +0000

    checking if we have printed the ethereum object

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
 import './App.css';
 
 export default function App() {
+
+  const checkIfWalletIsConnected = () => {
+    /*
+    * First make sure we have access to window.ethereum
+    */
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Make sure you have metamask!");
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
+  }
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
 
   const wave = () => {
     
@@ -15,11 +32,9 @@ export default function App() {
         <div className="header">
         This will look a little bit simple, but I don't like the front-end :)
         </div>
-
         <div className="bio">
         I am Mikel and thanks to Farza I learned solidity with a bunch of stuff more. Connect your Ethereum wallet and wave at me!
         </div>
-
         <button className="waveButton" onClick={wave}>
           Wave at Me ;D
         </button>
